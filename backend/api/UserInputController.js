d4780e1e1c9f3e639594342320f4c2bee78eafff
ww323 <ww323@uowmail.edu.au>
2022-01-20 08:25:23 +1100

    Added sanitisation and regex

--- backend/api/UserInputController.js
+++ backend/api/UserInputController.js
@@ -1,19 +1,38 @@
 const UserInputDAO = require("../dao/UserInputDAO.js");
+const createDOMPurify = require("dompurify");
+const {JSDOM} = require("jsdom");
+const e = require("express");
 
 class UserInputController {
   static async apiGetUserInput(req, res) {
     try {
-      const userIdentifier =
-        req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
-      const tempURL = req.protocol + "://" + req.get("host") + req.originalUrl;
-      const URL = tempURL.split(/[/ ]+/).pop();
-      const TimeOfInput = Date.now();
+      const windowEmulator = new JSDOM('').window;
+      const DOMPurify = createDOMPurify(windowEmulator);
+      if (DOMPurify.isSupported) {
+        /*Sanitisation*/
+        const dirtyIdentifier = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+        const dirtyURL = req.originalUrl;
+        const dirtyTimeOfInput = Date.now();
+        const cleanIdentifier = DOMPurify.sanitize(dirtyIdentifier);
+        const cleanURL = DOMPurify.sanitize(dirtyURL).replace('/', '');
+        const cleanTimeOfInput = DOMPurify.sanitize(dirtyTimeOfInput);
+        
+        /*Regex*/
+        if (/[a-f0-9]{20}$/i.exec(cleanURL) && cleanURL.length == 20) {
+          const UserInputResponse = await UserInputDAO.postUserInput(
+            cleanIdentifier,
+            cleanURL,
+            cleanTimeOfInput
+          );
+        }
+        else {
+          throw "Regex failed";
+        }
+      }
+      else {
+        throw "Sanitisation failed";
+      }
 
-      const UserInputResponse = await UserInputDAO.postUserInput(
-        userIdentifier,
-        URL,
-        TimeOfInput
-      );
       res.json({ status: "success" });
     } catch (e) {
       res.json({ status: "fail", cause: e });
