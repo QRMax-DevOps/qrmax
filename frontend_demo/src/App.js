a061a5728902df4936fc767c9e22096af985717a
MarcusHickey <marcus.hickey@outlook.com.au>
2022-01-20 18:01:45 +1100

    Front-end Update
    Polished and (mostly) completed the login and response screens (face only).

--- frontend_demo/src/App.js
+++ frontend_demo/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Routes, Route} from "react-router-dom";
 
 import Home from './components/Home';
-import Login from './components/Login';
+import Load_LoginScreen from './components/Login';
 import ResponseGood from './components/Response_Good';
 import ResponseBad from './components/Response_Bad';
 
@@ -14,7 +14,7 @@ function App() {
 	return (
 		<Routes>
 			<Route exact path="/" element={<Home/>}/>
-			<Route path="/login" element={<Login/>}/>
+			<Route path="/login" element={Load_LoginScreen()}/>
 			<Route path="/response_good" element={<ResponseGood/>}/>
 			<Route path="/response_bad" element={<ResponseBad/>}/>
 		</Routes>
--- frontend_demo/src/components/Response_Good.js
+++ frontend_demo/src/components/Response_Good.js
@@ -1,36 +1,39 @@
 import React from "react";
 import {Container, Row, Col, Image} from 'react-bootstrap';
 import GreenTickImage from '../graphics/tick.png'
+import Button from "./Custom_Button.js";
 
 class ResponseGood extends React.Component {
 	
  render() {
          return (
              <div>
-				<Container className="border border-dark" style={{margin: 0, position: "absolute", height:"90%", width:"70%", top: "50%", left: "50%", transform: "translate(-50%, -50%)",  minWidth:"500px", minHeight:"908px"}}>
-					<Col className="text-center d-flex flex-column justify-content-between" style={{height:"100%", padding:"10px"}}>
-						<Row className="border border-primary text-center" style={{alignItems:'flex-start', padding:"10px"}}>
-							<div className="border border-primary text-center" style={{marginTop:"100px"}}>
-								<Image src={GreenTickImage} style={{width:"50%", display:"block", marginLeft:"auto", marginRight:"auto"}}/>
+				<Container id="MainBox_Out" style={{margin: 0, position: "absolute", height:"100%", width:"70%", top: "50%", left: "50%", transform: "translate(-50%, -50%)",  minWidth:"500px", minHeight:"775px"}}>
+					<Col id="MainBox_In" className="text-center d-flex flex-column justify-content-between" style={{height:"100%", padding:"10px"}}>
+					
+						<Row id="Top" className="text-center" style={{alignItems:'flex-start', margin:"0"}}>
+							<div id="TopRow" style={{marginTop:"50px"}}>
+								<Image src={GreenTickImage} style={{width:"250px", display:"block", marginLeft:"auto", marginRight:"auto"}}/>
 								<br/>
-								<p style={{fontSize:"30px", fontWeight:"bold", color:"green"}}>Your input has<br/>been received!</p>
 							</div>
+							<p style={{margin:"0", fontSize:"30px", fontWeight:"bold", color:"green"}}>Your input has<br/>been received!</p>
 						</Row>
 						
-						<Row className="border border-warning text-center" style={{alignItems:'center'}}>
-							<div className="border border-warning text-center" style={{marginTop:"100px"}}>
-								<p style={{fontSize:"20px", fontWeight:"bold", color:"gray"}}>
+						<Row id="Middle" style={{alignItems:'center'}}>
+							<div className="text-center">
+								<p style={{fontSize:"20px", fontWeight:"500", color:"gray"}}>
 									Your opinion is valuable<br/>
 									and will help us improve<br/>
 									this service.
 								</p>
+								<br/>
+								<Button id="FeedbackButton" style={{height:"70px", width:"250px"}}>Give Feedback</Button>
 							</div>
-							<button style={{height:"70px"}}>Give Feedback</button>
 						</Row>
 						
-						<Row className="border border-success text-center" style={{alignItems:'flex-end'}}>
-							<div className="border border-success text-center" style={{marginTop:"100px"}}>
-								<p style={{color:"grey"}}>
+						<Row id="Bottom" style={{alignItems:'flex-end'}}>
+							<div className="text-center" style={{marginTop:"0", width:"350px", margin:"auto"}}>
+								<p style={{color:"grey", fontWeight:"500"}}>
 									We acknowledge the Traditional Custodians of the Land, and pay respects to their Elders past and present.
 								</p>
 							</div>
