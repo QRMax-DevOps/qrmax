441a3331bc5609fd1e27c4ecb6a0ff88e8aaf95d
jd970 <jd970@uowmail.edu.au>
2022-01-22 15:12:43 +1100

    minor changes (/QR/*)

--- backend/api/UserInput.route.js
+++ backend/api/UserInput.route.js
@@ -6,6 +6,5 @@ const router = express.Router();
 router
     .route("/")
     .get(UserInputController.apiGetUserInput)
-    //.post(UserInputController.apiPostUserInput)
 
 module.exports = router;
