e2283d77f48914faf741a16d7b21a88b28602ed4
jd970 <70502984+jd970@users.noreply.github.com>
2022-03-10 11:37:56 +1100

    storeController try-catch

--- backend/api/controller/StoreController.js
+++ backend/api/controller/StoreController.js
@@ -2,84 +2,103 @@ const CompanyDao = require("../../dao/CompanyDAO.js");
 
 class StoreController {
     static async list(req, res){
-        //get company name
-        const company = req.body.company;
-        //check if comapny exists
-        if(await CompanyDao.checkCompany(company)){
-            //get all stores list
-            const rjson = await CompanyDao.getList(company);
-            //return list as json
-            res.json(rjson);
+        try{
+            //get company name
+            const company = req.body.company;
+            //check if comapny exists
+            if(await CompanyDao.checkCompany(company)){
+                //get all stores list
+                const rjson = await CompanyDao.getList(company);
+                //return list as json
+                res.json(rjson);
+            }
+            else{
+                res.json({status:"failure", cause:"no such company"});
+            }
+            return res;
         }
-        else{
-            res.json({status:"failure", cause:"no such company"});
+        catch(e){
+            res.json({status:"failure", cause:e})
         }
-        return res;
     }
 
     static async register(req, res){
-        //get company, store
-        const company = req.body.company;
-        const store = req.body.store;
-        //ensure company exists
-        if(await CompanyDao.checkCompany(company)){
-            //check to ensure store dosent exists
-            if(!await CompanyDao.checkStore(company, store)){
-                CompanyDao.addStore(company, store)
-                res.json({status:"success"})
+        try{
+            //get company, store
+            const company = req.body.company;
+            const store = req.body.store;
+            //ensure company exists
+            if(await CompanyDao.checkCompany(company)){
+                //check to ensure store dosent exists
+                if(!await CompanyDao.checkStore(company, store)){
+                    CompanyDao.addStore(company, store)
+                    res.json({status:"success"})
+                }
+                else{
+                    res.json({status:"failure", cause:"store already exists"});
+                }
             }
             else{
-                res.json({status:"failure", cause:"store already exists"});
+                res.json({status:"failure", cause:"no such company"});
             }
         }
-        else{
-            res.json({status:"failure", cause:"no such company"});
+        catch(e){
+            res.json({status:"failure", cause:e})
         }
     }
 
     static async patch(req, res){
-        const company = req.body.company;
-        const store = req.body.store
-        const fields = req.body.fields;
-        const values = req.body.values;
+        try{
+            const company = req.body.company;
+            const store = req.body.store
+            const fields = req.body.fields;
+            const values = req.body.values;
 
-        const farray = fields.split(',');
-        const varray = values.split(',');
-        //check if store exists
-        if(await CompanyDao.checkStore(company, store)){
-            //check if fields exists
-            if(await CompanyDao.checkFields(company, store, farray)){
-                //update field with value
-                CompanyDao.patchStore(company, store, farray, varray);
-                res.json({status:"success"});
-            }
+            const farray = fields.split(',');
+            const varray = values.split(',');
+            //check if store exists
+            if(await CompanyDao.checkStore(company, store)){
+                //check if fields exists
+                if(await CompanyDao.checkFields(company, store, farray)){
+                    //update field with value
+                    CompanyDao.patchStore(company, store, farray, varray);
+                    res.json({status:"success"});
+                }
+                else{
+                    res.json({status:"failure", cause:'no such field'});
+                }
+            }   
             else{
-                res.json({status:"failure", cause:'no such field'});
+                res.json({status:"failure", cause:'no such account'});
             }
-        }   
-        else{
-            res.json({status:"failure", cause:'no such account'});
+        }
+        catch(e){
+            res.json({status:"failure", cause:e})
         }   
     }
 
     static async delete(req, res){
-        const company = req.body.company;
-        const store = req.body.store;
-        if(await CompanyDao.checkCompany(company)){
-            //check to ensure store dose exists
-            if(await CompanyDao.checkStore(company, store)){
-                CompanyDao.deleteStore(company, store)
-                res.json({status:"success"})
+        try{
+            const company = req.body.company;
+            const store = req.body.store;
+            if(await CompanyDao.checkCompany(company)){
+                //check to ensure store dose exists
+                if(await CompanyDao.checkStore(company, store)){
+                    CompanyDao.deleteStore(company, store)
+                    res.json({status:"success"})
+                }
+                else{
+                    res.json({status:"failure", cause:"no such store"});
+                }
             }
             else{
-                res.json({status:"failure", cause:"no such store"});
+                res.json({status:"failure", cause:"no such company"});
             }
         }
-        else{
-            res.json({status:"failure", cause:"no such company"});
+        catch(e){
+            res.json({status:"failure", cause:e})
         }
-    }
+    }   
 }
-
 
 module.exports = StoreController;
