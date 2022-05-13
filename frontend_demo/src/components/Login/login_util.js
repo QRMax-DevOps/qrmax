e2451418d88d3f0a853b08017613a16cc3febfc0
MarcusHickey <marcus.hickey@outlook.com.au>
2022-05-13 11:34:26 +1000

    F.E. - Error Demo

--- frontend_demo/src/components/Login/login_util.js
+++ frontend_demo/src/components/Login/login_util.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
 
 export function getNiceError(input) {
-	
-	var json = JSON.parse(input);
-	
 	console.log(input);
-	
-	switch(json.cause.toLowerCase()) {
-		case 'incorrect password': return 'Incorrect details. Please try again.';
-		case 'no such company': return "This company does not exist.";
-		case 'no such account': return "Username does not exist in company records.";
-		default: return 'Incorrect details. Please try again.';
+	try {
+		var json = JSON.parse(input);
 	}
+	catch(err) {
+		return input;
+	}
+	
+	
+	
+	if(json.cause) {
+		switch(json.cause.toLowerCase()) {
+			case 'incorrect password': return 'Incorrect details. Please try again.';
+			case 'no such company': return "This company does not exist.";
+			case 'no such account': return "Username does not exist in company records.";
+			default: return 'Incorrect details. Please try again.';
+		}
+	}
+	else if (json.error){
+		return 'An API error has occured. Please contact QRMAX administration.';
+	}
+	else {
+		return input;
+	}
+	
+	
 }
