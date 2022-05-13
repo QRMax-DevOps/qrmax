468e6a95280ce9e6a01accc95e370a287fb1b4d2
ww323 <ww323@uowmail.edu.au>
2022-05-13 11:49:29 +1000

    Added Cors

--- backendv2/server.js
+++ backendv2/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require("cors");
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
@@ -13,6 +14,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
+app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 
 //User input
