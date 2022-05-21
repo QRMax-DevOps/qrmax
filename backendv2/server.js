d724874162dacf3e98d67854cc702bc7fea7ac24
ww323 <92781412+ww323@users.noreply.github.com>
2022-05-21 13:22:02 +1000

    Fixed file transfer issue

--- backendv2/server.js
+++ backendv2/server.js
@@ -17,8 +17,9 @@ const app = express()
 
 app.use(express.json())
 app.use(cors());
-app.use(express.json({limit: '50mb', extended: false}));
-app.use(express.urlencoded({limit: "50mb", extended: false}));
+const bodyParser = require('body-parser')
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 //User input
 app.use("/api/v2/QR", require('./routes/QRRoutes'));
