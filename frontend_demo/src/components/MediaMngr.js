32f6ee4c875f3f90692fdcd05ed39ae6ef22d4c6
XFormulaBOMB <70363410+XFormulaBOMB@users.noreply.github.com>
2022-05-23 16:19:06 +1000

    Update MediaMngr.js

--- frontend_demo/src/components/MediaMngr.js
+++ frontend_demo/src/components/MediaMngr.js
@@ -4,15 +4,16 @@
  *  - Full name: Cody Spicer
  *  - Student ID: 6486125 */
 
-import React, {Component} from 'react';
-import './MediaMngr.css';
-import { ImageToBase64 } from '../services/utilities/base64_util';
+import React, {Component} from 'react'; //including the reactjs package
+import './MediaMngr.css'; //including the media manager CSS styling
+import { ImageToBase64 } from '../services/utilities/base64_util'; //importing package to convert image to a string
 
-import Sidebar from './Sidebar';
-import { handleDisplay } from '../services/middleware/display_mw';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
+import Sidebar from './Sidebar'; //including the navigation sidebar
+import { handleDisplay } from '../services/middleware/display_mw'; //including the middleware to send requests and recieve data
+import { Dropdown, DropdownButton } from 'react-bootstrap'; //includinf package for dropdown button
 
 class MediaMngr extends Component {
+    //initializing a constructor
     constructor(props) {
         super(props);
         this.selectMedia = this.selectMedia.bind(this);
@@ -25,9 +26,7 @@ class MediaMngr extends Component {
         this.fetchMedia = this.fetchMedia.bind(this);
         this.selectDisplay = this.selectDisplay.bind(this);
     }
-    state = {
-        currentCompany: "mediaCompany",
-        currentStore: "mediaStore",
+    state = { //initializing the state
         currentDisplay: "None Selected",
         currentObj: {media: [{mediaName: ''},{TTL: '0'}]}, 
         currentObjDisplay: {displays: [{displayName: ""}]},
@@ -39,55 +38,47 @@ class MediaMngr extends Component {
         imgString: null,
         mediaCount: 0
     }
-
+    //retrieveing currentDisplay
     getCurrentDisplayObj(){
         return this.state.currentDisplay;
     }
-
+    //retrieveing selectedMedia
     getSelectedMedia(){
         return this.state.currentObj.media[this.state.selectedMedia];
     }
-
+    //changing selected display to that selected from the list
     selectDisplay(e){
         this.setState({
             currentDisplay: e.target.innerHTML
         });
         this.getMedia(e);
     }
-
+    //changing selected media to that selected from the list
     selectMedia(e) {
-        if(e.target.innerHTML == "+New Media"){
-            this.setState({
-                selectedMedia: e.target.value,
-                mediaInput: e.target.innerHTML,
-                mediaUpdate: e.target.innerHTML
-            });
-        }else{
-            this.setState({
-                selectedMedia: e.target.value,
-                mediaInput: e.target.innerHTML,
-                mediaUpdate: e.target.innerHTML,
-            });
-        }
+        this.setState({
+            selectedMedia: e.target.value,
+            mediaInput: e.target.innerHTML,
+            mediaUpdate: e.target.innerHTML,
+        }); 
     }
-
+    //fetching media once a display has been chosen
     getMedia(e){
         var data;
         this.fetchMedia("post", data = {company: this.state.currentCompany, store: this.state.currentStore, display: e.target.innerHTML}, 1);
     }
-
+    //changing media input based on whats entered into the media name field
     changeCurrentMediaInput(e) {
         this.setState({
             mediaInput: e.target.value
         })
     }
-
+    //changing media length based on whats entered into the media length field
     changeCurrentMediaLength(e) {
         this.setState({
             mediaLength: e.target.value
         })
     }
-
+    //changing the selected file based on whats entered into the file part of entry form
     async setSelectedFile(e) {
         const file = e.target.files[0];
          const base64 = await this.getBase64(file);
@@ -95,7 +86,7 @@ class MediaMngr extends Component {
              imgString: base64
          })
     }
-
+    //sending a request to patch media
     updateMedia(){
         if(this.state.selectedMedia < this.state.mediaCount){
         var newName = this.getNewName();
@@ -105,22 +96,23 @@ class MediaMngr extends Component {
         console.log(data);
         window.location.reload();
         }else{
+            //ensuring the user can't update a non existent media 
             console.log("can't update new media")
             alert("You can't update +New Media")
         }
     }
-
+    //retrieving the name input into the media name field
     getNewName() {
         return this.state.mediaInput;
     }
-
+    //sending a request to put a created media
     createMedia() {
         let newName = this.getNewName();
         var data = {company: this.state.currentCompany, store: this.state.currentStore, display: this.state.currentDisplay, mediaName: newName, mediaFile: this.state.imgString, TTL: this.state.mediaLength};
         this.fetchMedia("put", data, 1);
         window.location.reload();
     }
-
+    //sending a request to delete a selected media
     deleteMedia() {
         if(this.state.selectedMedia < this.state.mediaCount){
             var data = {company: this.state.currentCompany, store: this.state.currentStore, display: this.state.currentDisplay, mediaName: this.state.mediaInput};
@@ -132,7 +124,7 @@ class MediaMngr extends Component {
         }
     }
 
-
+    //function to handle requests to the api and retrieve responses
     fetchMedia(type, data, objectCount) {
         var url = "http://localhost:80/";
 
@@ -156,12 +148,13 @@ class MediaMngr extends Component {
             if(response[0] !== null){
                 clearInterval(interval);
                 me.setState({loading:false});
-
                 if(response[0] === true){
                     var json = JSON.parse(response[1]);
+                    //fetching the displays of the appropriate store
                     if(objectCount == 1){
                     me.setState({currentObj: json});
                     }
+                    //fetching media for a selected display
                     else{
                     me.setState({currentObjDisplay: json});
                     }
@@ -175,7 +168,7 @@ class MediaMngr extends Component {
             }
         }, 500);
     }
-
+    //converting an entered file into a string which is easier to store
     getBase64(file) {
         return new Promise(function(resolve, reject){
 
@@ -190,7 +183,7 @@ class MediaMngr extends Component {
             }
         });
     }
-
+    //an initial request made to fetch displats
     componentDidMount() {
         var data;
         this.fetchMedia("post", data = {company: this.state.currentCompany, store: this.state.currentStore}, 0);
@@ -205,10 +198,10 @@ class MediaMngr extends Component {
                     <Sidebar/>
                 </div>
                 <div className="main-container">
-                    <div className='ViewerTitle'>Media Management</div>
-                    <h5 id='selected-display-header'>Showing Display: {this.getCurrentDisplayObj()}</h5>
+                    <div className='ViewerTitle'>Media Management</div> {/* main title */}
+                    <h5 id='selected-display-header'>Showing Display: {this.getCurrentDisplayObj()}</h5> {/* shows selected display */}
 
-                    <DropdownButton id="displayDrop" title="Display">
+                    <DropdownButton id="displayDrop" title="Display"> {/* A dropdown box that allows the user to select a display */}
                             {this.state.currentObjDisplay.displays.map((val, key) => {
                                 return (
                                     <Dropdown.Item key={key} value={key} onClick={this.selectDisplay}>{val.displayName}</Dropdown.Item>
@@ -217,6 +210,7 @@ class MediaMngr extends Component {
                     </DropdownButton>
 
                     <div id="styled-container">
+                        {/* a list of all media for the chosen display */}
                         <ul id='media-list'>
 
                             {this.state.currentObj.media.map((val, key) => {
@@ -230,17 +224,21 @@ class MediaMngr extends Component {
                         </ul>
                         <div id='settings-box'>
                             <h5 id='settings-box-header'></h5>
+                            {/* an input to allow the entry of a media name */}
                             <input id='name-field' type='text' placeholder='Media Name' onChange={this.changeCurrentMediaInput} value={this.state.mediaInput}></input>
-
+                            {/* an input to allow the entry of media length in seconds */}
                             <input id='length-field' type='number' placeholder='Media Length (seconds)' onChange={this.changeCurrentMediaLength} value={this.state.mediaLength}></input>
-
+                             {/* an input to allow the entry of a media file */}
                             <input type="file" onChange={this.setSelectedFile}/>
 
                         </div>
+                        {/* a button that will alow the information of a selected media to be updated */}
                         <button type="submit" id="update-button" className='buttons' onClick={this.updateMedia}>Update Media</button>
                         <br/>
+                        {/* a button that will send the information to be turned into a new media */}
                         <button type="button" id="create-button" className='buttons' onClick={this.createMedia}>Create New Media</button>   
                         <br/>
+                        {/* a button that will delete the selected media */}
                         <button type="button" id="delete-button" className='buttons' onClick={this.deleteMedia}>Delete Media</button>
                         </div>
                         
