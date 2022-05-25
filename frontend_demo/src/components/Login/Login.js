854cbcdd8f81af8ea54ad2b21b05497f72474cfb
MarcusHickey <marcus.hickey@outlook.com.au>
2022-05-25 18:53:26 +1000

    Patching out "localhost" login feature

--- frontend_demo/src/components/Login/Login.js
+++ frontend_demo/src/components/Login/Login.js
@@ -1,82 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import Image from 'react-bootstrap/Image'
-import ReactDOM from 'react-dom'
-import { Route , withRouter} from 'react-router-dom';
-import {Container, Row, Col, Form, FormControl, InputGroup} from 'react-bootstrap';
+import React from "react";
 
-import {log, getApiURL} from '../../services/core_mw';
-import {RunFetch_Login} from '../../services/middleware/login_mw';
-import {saveLoginToken} from '../../services/utilities/auth_util';
-import {getNiceError} from './login_util';
+import {log, getApiURL} from "../../services/core_mw";
+import {RunFetch_Login} from "../../services/middleware/login_mw";
+import {saveLoginToken, clearSessionData} from "../../services/utilities/auth_util";
+import {getNiceError} from "./login_util";
 
-import Img1 from '../../graphics/updated_logo.PNG'
-import Homepage from '../Homepage';
+import Img1 from "../../graphics/updated_logo.PNG";
 
-import '../../style.css';
-import './login_style.css'
+import "../../style.css";
+import "./login_style.css";
 
-import LoadingGif from '../../graphics/loading_login.gif'
+import LoadingGif from "../../graphics/loading_login.gif";
 
+
+
+
+/*Used to store networking information (I.e., responses from the server)
+ *Simply stored here for easy access*/
 var GLOBAL = [null,null];
-
-function checkDetails(isCompany, companyName, userName, passCode) {
-	let valid = true;
-		
-	if(!companyName) {
-		valid = false;
-		log('Login Error: Invalid COMPANY (provided: \''+companyName+'\')');
-		GLOBAL = [false, "(Please provide a company name)"];
-	}
-	if(!userName && !isCompany) {
-		valid = false;
-		log('Login Error: Invalid USERNAME (provided: \''+userName+'\')');
-		GLOBAL = [false, "(Please provide a username)"];
-	}
-	if(!passCode) {
-		valid = false;
-		log('Login Error: Invalid PASSWORD (provided: \''+passCode+'\')');
-		if(companyName) { GLOBAL = [false, "(Please provide a password)"]; }
-		
-	}
-	return valid;
-}
-
 function resetGlobal() { GLOBAL = [null,null]; }
 
+function checkDetails(isCompany, companyName, userName, passCode) {
+    let valid = true;
+    
+    if(!companyName) {
+        valid = false;
+        log("Login Error: Invalid COMPANY (provided: '"+companyName+"')");
+        GLOBAL = [false, "(Please provide a company name)"];
+    }
+    if(!userName && !isCompany) {
+        valid = false;
+        log("Login Error: Invalid USERNAME (provided: '"+userName+"')");
+        GLOBAL = [false, "(Please provide a username)"];
+    }
+    if(!passCode) {
+        valid = false;
+        log("Login Error: Invalid PASSWORD (provided: '"+passCode+"')");
+        if(companyName) { GLOBAL = [false, "(Please provide a password)"]; }	
+    }
+    return valid;
+}
 
-
-
-/*
-
-if (typeof window !== 'undefined') {
-    var path = location.protocol + '//' + location.host + '/someting'; // (or whatever)
-  } else {
-    // work out what you want to do server-side...
-  }
-
-*/
-
-
+//Primary class. The parent object for the website.
 export default class Page extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
+        
+        clearSessionData();
 		
-		 //When loading==true, show "LoadingScreen"
-		this.state = {
-			loading : false,
-			passwordBox : '',
-			companyBox : '',
-			usernameBox : '',
-			rememberMeCheck : false,
-			companyAccount : false,
-			localhost : false,
-			loginError : '',
-		}
+        this.state = {
+            loading : false, //When loading==true, show "LoadingScreen"
+            passwordBox : '',
+            companyBox : '',
+            usernameBox : '',
+            rememberMeCheck : false,
+            companyAccount : false,
+            loginError : '',
+        };
 		
-		var recalled_username = localStorage.getItem('login_recall_username');
-		var recalled_company = localStorage.getItem('login_recall_company');
-		var recalled_companyChecked = localStorage.getItem('login_recall_companyChecked');
-		var recalled_localhostChecked = localStorage.getItem('login_recall_localhostChecked');
+		var recalled_username = localStorage.getItem("login_recall_username");
+		var recalled_company = localStorage.getItem("login_recall_company");
+		var recalled_companyChecked = localStorage.getItem("login_recall_companyChecked");
 		
 		if(recalled_username != null && recalled_username.length > 0) {
 			this.state.usernameBox = recalled_username;
@@ -90,10 +74,6 @@ export default class Page extends React.Component {
 			this.state.companyAccount = true;
 			this.state.rememberMeCheck = true;
 		}
-		if(recalled_localhostChecked != null) {
-			this.state.localhost = true;
-			this.state.rememberMeCheck = true;
-		}
 		
 		this.updateRememberedValues = this.updateRememberedValues.bind(this);
 		this.setRememberMe = this.setRememberMe.bind(this);
@@ -103,9 +83,15 @@ export default class Page extends React.Component {
 	}
 	
 	showHelp() {
-		alert("Forgotten your password? No worries!\n\nWe suggest that you contact your organisation's administrator to assist you with the password reset process.\nIf you are the administrator, then please contact QRMAX support and we will guide you through this process.");
+		alert("Forgotten your password? No worries!\n"+
+			"\nWe suggest that you contact your organisation's"+
+			"administrator to assist you with the password reset"+
+			"process.\nIf you are the administrator, then please"+
+			"contact QRMAX support and we will guide you through"+
+			"this process.");
 	}
 	
+	// Used to update the "remember me" status and make changes as needed
 	setRememberMe(newState) {
 		if(newState === true) {
 			this.setState({rememberMeCheck : true});
@@ -117,6 +103,8 @@ export default class Page extends React.Component {
 		}
 	}
 	
+	/* Used to actually "remember" the values. If called upon, 
+	 * this information will be saved to local storage */
 	updateRememberedValues() {
 		localStorage.setItem("login_recall_username", this.state.usernameBox);
 		localStorage.setItem("login_recall_company", this.state.companyBox);
@@ -125,48 +113,36 @@ export default class Page extends React.Component {
 			localStorage.setItem("login_recall_companyChecked", true);
 		}
 		else {
-			localStorage.removeItem('login_recall_companyChecked');
-		}
-		
-		if(this.state.localhost === true) {
-			localStorage.setItem("login_recall_localhostChecked", true);
-		}
-		else {
-			localStorage.removeItem('login_recall_localhostChecked');
+			localStorage.removeItem("login_recall_companyChecked");
 		}
 	}
 	
 	clearRememberedValues() {
-		localStorage.removeItem('login_recall_username');
-		localStorage.removeItem('login_recall_company');
-		localStorage.removeItem('login_recall_companyChecked');
-		localStorage.removeItem('login_recall_localhostChecked');
+		localStorage.removeItem("login_recall_username");
+		localStorage.removeItem("login_recall_company");
+		localStorage.removeItem("login_recall_companyChecked");
 	}
 	
 	navigateToHome() {
-		sessionStorage.setItem('companyName',this.state.companyBox);
+		sessionStorage.setItem("companyName",this.state.companyBox);
 		
 		if(this.state.companyAccount) {
-			sessionStorage.setItem('username',this.state.companyBox);
+			sessionStorage.setItem("username",this.state.companyBox);
 		}
 		else {
-			sessionStorage.setItem('username',this.state.usernameBox);
+			sessionStorage.setItem("username",this.state.usernameBox);
 		}
-		
-		sessionStorage.setItem('isLocalhost',false);
-		if(this.state.localhost===true) { 
-			sessionStorage.setItem('isLocalhost',true);
-		}
-		
-		sessionStorage.setItem('isCompanyAccount',this.state.companyAccount);
+		sessionStorage.setItem("isCompanyAccount",this.state.companyAccount);
 		if(this.state.companyAccount===true) { 
-			sessionStorage.setItem('isCompanyAccount',this.state.companyAccount);
+			sessionStorage.setItem("isCompanyAccount",this.state.companyAccount);
 		}
 
 		saveLoginToken(JSON.parse(GLOBAL[1]).token);
-		window.location.href = window.location.protocol + '//' + window.location.host + '/homepage';
+		
+		window.location.href = window.location.protocol + "//" + window.location.host + "/homepage";
 	}
 	
+	//On component update, update "remembered" values.
 	componentDidUpdate(prevProps, prevState) {
 		if(prevState!==this.state && this.state.rememberMeCheck === true) {
 			this.updateRememberedValues();
@@ -178,82 +154,67 @@ export default class Page extends React.Component {
 		if(!checkboxType) {
 			this.setState({
 				[target.name]: target.value,
-				loginError : ''
+				loginError : ""
 			});
 		}
 		else {
-			this.setState({[target.name] : target.checked})
-			
+			this.setState({[target.name] : target.checked});
 			
 			if(target.name === 'companyAccount') {
 				if(target.checked === true && this.state.rememberMeCheck === true) {
 					localStorage.setItem("login_recall_companyChecked", true);
 				}
 				else {
-					localStorage.removeItem('login_recall_companyChecked');
-				}
-			}
-			
-			if(target.name === 'localhost') {
-				if(target.checked === true && this.state.rememberMeCheck === true) {
-					localStorage.setItem("login_recall_localhostChecked", true);
-				}
-				else {
-					localStorage.removeItem('login_recall_localhostChecked');
+					localStorage.removeItem("login_recall_companyChecked");
 				}
 			}
 		}
 	}
 	
 	SubmitLoginRequest() {
-		this.setState({loading: true});
-		
 		resetGlobal();
-		
+		this.setState({loading: true});
 		const me = this;
-
 		var timer = { eclapsed: 0 };
-		
 		var detailsValid = false;
-		
-		var response = {dataSent:false,username:null,passcode:null}
+		var response = {dataSent:false,username:null,passcode:null};
 		
 		var interval = setInterval(function() {
 			++timer.eclapsed;
-			//console.log(timer, me.state.loading);
 			
-			//after 0.5 seconds
-			if(timer.eclapsed >= 1 && response.dataSent==false) {
-				//submit information
-				response.dataSent = true;
-				
-				//do fetch
-					
-				detailsValid = checkDetails(me.state.companyAccount, me.state.companyBox, me.state.usernameBox, me.state.passwordBox);
-
-				
+			//after 0.5 seconds, submit info
+			if(timer.eclapsed >= 1 && response.dataSent===false) {
+				response.dataSent = true;	
+				detailsValid = checkDetails(
+					me.state.companyAccount,
+					me.state.companyBox,
+					me.state.usernameBox,
+					me.state.passwordBox
+				);
 				if(detailsValid) {
-					var data = {companyInput:me.state.companyBox, usernameInput:me.state.usernameBox, passwordInput:me.state.passwordBox};
-					
-					//console.log("localhost===",me.state.localhost);
-					//console.log("getApiURL===",getApiURL(me.state.localhost));
-					
+					var data = {
+						companyInput:me.state.companyBox,
+						usernameInput:me.state.usernameBox,
+						passwordInput:me.state.passwordBox
+					};
 					if(me.state.companyAccount) {
 						data.usernameInput = me.state.companyBox;
 					}
 					
-					
-					RunFetch_Login(getApiURL(me.state.localhost), me.state.companyAccount, data, GLOBAL);
+					//Attempt login
+					RunFetch_Login(
+						getApiURL(false),
+						me.state.companyAccount,
+						data,
+						GLOBAL
+					);
 				}
-
 			}
-			
-			else if(response.dataSent==true && GLOBAL[0] != null) {
+			else if(response.dataSent===true && GLOBAL[0] != null) {
 				me.setState({loading : false});
 				clearInterval(interval);
 				
-				if(GLOBAL[0]==false) {
-					
+				if(GLOBAL[0]===false) {
 					if(!detailsValid) {
 						me.setState({loginError : GLOBAL[1]});
 					}
@@ -263,17 +224,13 @@ export default class Page extends React.Component {
 					}
 				}
 				
-				if(GLOBAL[0]==true) {
+				if(GLOBAL[0]===true) {
 					me.navigateToHome();
 				}
-				
-				//Check for errors
-				//Visualise errors (username, password, check console, etc.)
-				
 			}
 
 			//timeout after 3 seconds
-			if(timer.eclapsed == 24) {
+			if(timer.eclapsed >= 24) {
 				console.log("timer timeout!");
 				me.setState({loading : false});
 				
@@ -290,7 +247,7 @@ export default class Page extends React.Component {
 
 					<div id="leftbox" className="alignmentCheck">
 						<div id="innerbox_left">
-							<img style={{marginBottom:"20px"}} id="logo" src={Img1}/>
+							<img style={{marginBottom:"20px"}} id="logo" src={Img1} alt=""/>
 							<p id="subtitle">
 								Helping businesses build<br/>
 								contactless kiosks since 2022!<br/><br/>
@@ -301,7 +258,7 @@ export default class Page extends React.Component {
 					<div id="rightbox" className="alignmentCheck">
 						{this.state.loading===true && 
 							<div id="LoadingContainer">
-							<img id="LoadingGraphic" src={LoadingGif}/>
+							<img id="LoadingGraphic" src={LoadingGif} alt=""/>
 							</div>
 						}
 						<div id="rightbox_inner">
@@ -319,10 +276,6 @@ export default class Page extends React.Component {
 									<div id="RememberMe_Container" className="form-check">
 										<input name="companyAccount" type="checkbox" className="form-check-input" id="CompanyAccountCheck" checked={this.state.companyAccount} onChange={e => this.handleChange(e, true)}/>
 										<label style={{color:'white', marginLeft:'10px', marginTop:'8px'}}>Company Account</label>
-									</div>
-									<div id="RememberMe_Container" className="form-check">
-										<input name="localhost" type="checkbox" className="form-check-input" id="LocalHostCheck" checked={this.state.localhost} onChange={e => this.handleChange(e, true)}/>
-										<label style={{color:'white', marginLeft:'10px', marginTop:'8px'}}>Localhost</label>
 									</div>
 								</div>
 							  
@@ -375,13 +328,22 @@ export default class Page extends React.Component {
 								  </div>
 								  <div id="Checkbox_Container">
 									  <div id="RememberMe_Container" className="form-check">
-										<input style={{width:"22px"}} type="checkbox" checked={this.state.rememberMeCheck} onChange={() => this.setRememberMe(!this.state.rememberMeCheck)} className="form-check-input"/>
-										<label style={{color:'white', marginLeft:'10px', marginTop:'8px'}}>Remember me</label>
+										<input
+											style={{width:"22px"}}
+											type="checkbox"
+											checked={this.state.rememberMeCheck}
+											onChange={() => this.setRememberMe(!this.state.rememberMeCheck)}
+											className="form-check-input"/>
+										<label style={{color:'white', marginLeft:'10px', marginTop:'8px'}}>
+											Remember me
+										</label>
 									  </div>
 								  </div>
 							</form>
 						</div>
-						<button type="button" onClick={this.SubmitLoginRequest} className="btn" id="submitButton">Submit</button>
+							<button type="button" onClick={this.SubmitLoginRequest} className="btn" id="submitButton">
+								Submit
+							</button>
 						</div>
 					</div>
 				  </div>
