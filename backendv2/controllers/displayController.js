40b015e9223e0c691a84894b532ab275458a6402
jd970 <70502984+jd970@users.noreply.github.com>
2022-06-02 02:46:30 +1000

    Update displayController.js

--- backendv2/controllers/displayController.js
+++ backendv2/controllers/displayController.js
@@ -85,8 +85,10 @@ const postDisplay = asyncHandler(async (req, res) => {
       displays[i].media[j] = await mediaModel.findById(displays[i].media[j]);
       displays[i].media[j] = displays[i].media[j].mediaName;
     }
-    displays[i].currentContent = await mediaModel.findById(displays[i].currentContent.media);
-    displays[i].currentContent = displays[i].currentContent.mediaName;
+    let currentContent = displays[i].currentContent;
+    currentContent.media = await mediaModel.findById(currentContent.media, {mediaName:1});
+    displays[i].currentContent = currentContent;
+    
   }
   res.status(200).json({status:"success",displays});
 });
