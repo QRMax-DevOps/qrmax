f23256e41047dbc48087f8fbc4a3b6f08ef1e746
Trentos1 <trentrussell97@gmail.com>
2022-06-01 16:34:29 +1000

    Fixing the second error, Jack <3

--- frontend_demo/src/components/Homepage.js
+++ frontend_demo/src/components/Homepage.js
@@ -436,7 +436,7 @@ class Homepage extends Component {
                             {this.state.currentObj.displays[this.state.selectedDisplay].media.map((val, key) => {
                                 return (
                                     <Draggable key={key} >
-                                        <QRCode className={this.state.qrStyle} value={"https://qrmax.app/inputresponse?company="+sessionStorage.companyName+"&store=demoStore1&display=display1&qrid=" + this.state.displayMedia.media[key].QRID}/>
+                                        <QRCode className={this.state.qrStyle} value={"https://qrmax.app/inputresponse?company="+sessionStorage.companyName+"&store="+this.state.storesObj.stores[this.state.selectedStore].store+"&display="+this.state.currentObj.displays[this.state.selectedDisplay].displayName+"&qrid=" + this.state.displayMedia.media[key].QRID}/>
                                     </Draggable>
                                     )
                                 })}
